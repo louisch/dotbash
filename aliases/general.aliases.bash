f7c50484f385752381686a82e6eba7f00dded34e
Louis Chan <louis058@gmail.com>
2011-06-10 13:03:54 +0100

    Added aliases

---
+++ aliases/general.aliases.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# List directory contents
+alias sl=ls
+alias ls='ls --color=auto'
+# Automatic colouring of results.
+alias la='ls -AF'
+# Show everything (apart from . and ..),
+# and classify everything.
+alias ll='ls -Alh'
+# Show everything, and list them in a list format.
+
+alias mkdir='mkdir -p'
+# Make parent directories automatically
+
+alias s='sudo'
+
+alias c='clear'
+# Clears the screen.
+
+alias h='history'
+
+alias q='exit'
+
+# Tree
+alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
---
+++ aliases/pacman.aliases.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+alias pacupg='sudo packer -Syu'
+# Full upgrade to all packages.
+alias pacin='sudo packer -S --noedit'
+alias pacinedit='sudo packer -S'
+alias pacins='sudo pacman -U'
+# Commands to install packages.
+alias pacre='sudo pacman -R'
+alias pacrem='sudo pacman -Runs'
+# Commands to remove packages.
+alias pacrep='sudo packer -Si'
+alias pacreps='sudo packer -Ss'
+# Commadns to search repositories.
+alias pacloc='pacman -Qi'
+alias paclocs='pacman -Qs'
+# Commands to search locally.
+alias pactarball='sudo packer -G'
+# Install from a tarball (AUR).
