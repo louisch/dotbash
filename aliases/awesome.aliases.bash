97f36e546d655f83dd6aa416b3cc5f8477a72c0d
Louis Chan <louis058@gmail.com>
2012-07-27 23:31:08 +0100

    Change awesome testing file name

--- aliases/awesome.aliases.bash
+++ aliases/awesome.aliases.bash
@@ -1,5 +1,5 @@
 # Awmtt is script for starting Xephyr for testing Awesome.
-alias awmtt-st="awmtt start -D 1 -C /home/louis/.config/awesome/test.rc.lua -S 1024x640"
+alias awmtt-st="awmtt start -D 1 -C /home/louis/.config/awesome/rc.lua.test -S 1024x640"
 alias awmtt-sp="awmtt stop"
 alias awmtt-spa="awmtt stop all"
 alias awmtt-r="awmtt restart"
